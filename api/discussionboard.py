dbed7da4d8e0b882cfcac54d53d781f787e1b167
ninaadkiran <ninaadkiran@gmail.com>
2023-10-31 01:12:39 +0000

    discussion board API change

--- api/discussionboard.py
+++ api/discussionboard.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'wS1qRtuSilAq'
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this to a strong secret key
 jwt = JWTManager(app)
 
 # Sample data for discussions, posts, and comments
@@ -36,13 +36,13 @@ def get_discussions():
     return jsonify(discussions)
 
 # API route to create a new post in a discussion
-@app.route('/discussions/<int:discussion_id>/posts', methods=['POST'])
+@app.route('/discussions/<discussion_title>/posts', methods=['POST'])
 @jwt_required
-def create_post(discussion_id):
+def create_post(discussion_title):
     data = request.get_json()
     if 'content' in data:
         new_post = {
-            'discussion_id': discussion_id,
+            'discussion_title': discussion_title,
             'content': data['content'],
             'created_by': get_jwt_identity()
         }
@@ -51,18 +51,18 @@ def create_post(discussion_id):
     return jsonify({'message': 'Invalid data'}), 400
 
 # API route to get all posts in a discussion
-@app.route('/discussions/<int:discussion_id>/posts', methods=['GET'])
-def get_posts_in_discussion(discussion_id):
-    discussion_posts = [post for post in posts if post['discussion_id'] == discussion_id]
+@app.route('/discussions/<discussion_title>/posts', methods=['GET'])
+def get_posts_in_discussion(discussion_title):
+    discussion_posts = [post for post in posts if post['discussion_title'] == discussion_title]
     return jsonify(discussion_posts)
 
 # API route to add a comment to a post
-@app.route('/discussions/<int:discussion_id>/posts/<int:post_id>/comments', methods=['POST'])
+@app.route('/discussions/<discussion_title>/posts/<int:post_id>/comments', methods=['POST'])
 @jwt_required
-def add_comment(discussion_id, post_id):
+def add_comment(discussion_title, post_id):
     data = request.get_json()
     if 'content' in data:
-        post = next((post for post in posts if post['discussion_id'] == discussion_id and post['post_id'] == post_id), None)
+        post = next((post for post in posts if post['discussion_title'] == discussion_title and post['post_id'] == post_id), None)
         if post:
             new_comment = {
                 'content': data['content'],
@@ -71,6 +71,5 @@ def add_comment(discussion_id, post_id):
             comments.append(new_comment)
             return jsonify({'message': 'Comment added successfully'}), 201
     return jsonify({'message': 'Invalid data'}), 400
-
 if __name__ == '__main__':
     app.run(debug=True)
